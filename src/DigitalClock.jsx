1788428478f2887803c677d202e005033fc8d3b2
VanderG-dev <VanderG.dev@gmail.com>
2024-09-17 12:47:48 +0600

    add background

---
+++ src/DigitalClock.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+
+function DigitalClock() {
+  return (
+    <>
+      <div className="clock">
+        <span className="numbers">12:31 PM</span>
+      </div>
+    </>
+  );
+}
+
+export default DigitalClock;
